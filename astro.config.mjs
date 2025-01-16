ad74ecda5b60e0fed7ccdca939ee1320025dd098
Bruno Alves <nito.ba.dev@gmail.com>
2025-01-16 15:08:26 -0300

    feat: add Vercel adapter and update dependencies in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   env: {
     schema: {
       USER_EMAIL: envField.string({ context: "server", access: "secret" }),
@@ -32,10 +35,13 @@ export default defineConfig({
       }),
     },
   },
+
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
   ],
+
+  adapter: vercel(),
 });
