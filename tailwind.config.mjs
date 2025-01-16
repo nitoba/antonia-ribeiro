ed1b6f84972e915e92f971273b8482382a32f458
Bruno Alves <nito.ba.dev@gmail.com>
2025-01-16 15:05:27 -0300

    feat: finish landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        logo: ["'Glacial'", ...defaultTheme.fontFamily.sans],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
